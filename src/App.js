ac763fb80cef5bfe3f701ee42031b4f48ddc29e2
Stanley Anyassor <anyassorstanley@gmail.com>
2023-03-01 13:20:35 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import { FrontPage } from './components/Front-page';
+
+function App() {
+  return (
+    <div className="App">
+      <FrontPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Front-page.js
@@ -0,0 +1,91 @@
+import { React, useEffect, useState } from "react";
+import InputAdornment from '@mui/material/InputAdornment';
+import TextField from '@mui/material/TextField';
+import IconButton from'@mui/material/IconButton';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Stack } from '@mui/system';
+import { Grid } from '@mui/material';
+import axios from "axios";
+import accessKey from "../key";
+
+export const FrontPage = () => {
+    const [searchValue, setSearchValue] = useState("");
+    const [images, setImages] = useState([]);
+    //const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        searchPhoto();
+    }, []);
+    const searchPhoto = () => {
+        axios.get(`https://api.unsplash.com/search/photos?per_page=12&client_id=${accessKey}&query=${searchValue}`)
+        .then((response) => {
+          const newImages = response ? response.data.results.map((result) => ({
+            id: result.id ? result.id : "No id",
+            description: result.description ? result.description : "No description",
+            nameOfPicture: result.alt_description ? result.alt_description : "No name of picture",
+            name: result.user.name ? result.user.name : "No name",
+            url: result.urls.regular ? result.urls.regular : "No url",
+          })) : 'https://images.unsplash.com/photo-1631630000000-0d0d0d0d0d0d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80';
+          setImages(newImages);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    const inputChanged = (event) => {
+        setSearchValue(event.target.value);
+      }
+  
+    return (
+        <div>
+            <AppBar position="static" >
+                <Toolbar>
+                    <Typography variant="h6" alignItems={"center"}>
+                        FIND ANY PHOTO
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Stack spacing={2} mt={10} direction="row" justifyContent="center" alignItems="center">
+                <TextField type={Text} label="Search photo" variant="outlined" name="search" onChange={inputChanged} onKeyDown={(event) => { 
+                    if (event.keyCode === 13)
+                        searchPhoto();
+                    }}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton onClick={() => setSearchValue(" ")}>
+                                    x
+                                </IconButton>
+                            </InputAdornment>
+                        )
+                    }}
+                    />
+                <Button variant="contained" onClick={searchPhoto}>Search</Button>
+            </Stack>
+            <br />
+            <br />
+            <Grid container spacing={{ xs: 4, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                {images.map((image) => (
+                    <Grid item xs={4} sm={4} md={4} key={image.id}>
+                        <img key={image.id} src={image.url} alt="Random" height={"300px"} width={"400px"} />
+                        <p>
+                            <span style={{ color: "red" }}>Description:</span>{" "}
+                            <h3>{image.description}</h3>
+                        </p>
+                        <p>
+                            <span style={{ color: "red" }}>Name of picture:</span>
+                            <h3>{image.nameOfPicture}</h3>
+                        </p>
+                        <p>
+                            <span style={{ color: "red" }}>Photographer's name:</span>
+                            <h3>{image.name}</h3>
+                        </p>
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    );
+}
