c06d0c50c90c4ff3457d0e79cdbb47ab4c6b58f2
Stanley Anyassor <anyassorstanley@gmail.com>
2023-03-14 09:53:44 +0200

    added some features

--- src/components/Front-page.js
+++ src/components/Front-page.js
@@ -1,7 +1,10 @@
 import { React, useCallback, useState } from "react";
+import "../App.css";
 import InputAdornment from '@mui/material/InputAdornment';
 import TextField from '@mui/material/TextField';
 import IconButton from'@mui/material/IconButton';
+import ShareIcon from '@mui/icons-material/Share';
+import DownloadIcon from '@mui/icons-material/Download';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -9,12 +12,19 @@ import Button from '@mui/material/Button';
 import { Stack } from '@mui/system';
 import { Grid } from '@mui/material';
 import axios from "axios";
-//import accessKey from "./key";
+import { Card } from '@mui/material';
+import { CardContent } from '@mui/material';
+import { CardMedia } from '@mui/material';
+import { CardActions } from '@mui/material';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 export const FrontPage = () => { 
     const [searchValue, setSearchValue] = useState("");
     const [images, setImages] = useState([]);
-    //console.log("key", process.env.REACT_APP_ACCESS_KEY)
     const accessKey = process.env.REACT_APP_ACCESS_KEY;
    
     const searchPhoto = useCallback(() => {
@@ -35,10 +45,16 @@ export const FrontPage = () => {
     }, [searchValue, accessKey]);
     const inputChanged = (event) => {
         setSearchValue(event.target.value);
-      }
-  
+      };
+
+    const sharePhoto = (imageurl, description) => {
+        const twitterUrl = `https://twitter.com/intent/tweet?text=${description}&url=${imageurl}`;
+        window.open(twitterUrl, "_blank");
+    }
+
     return (
         <div>
+
             <AppBar position="static" >
                 <Toolbar>
                     <Typography variant="h6" alignItems={"center"}>
@@ -68,22 +84,36 @@ export const FrontPage = () => {
             </Stack>
             <br />
             <br />
+
             <Grid container spacing={{ xs: 4, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                 {images.map((image) => (
                     <Grid item xs={4} sm={4} md={4} key={image.id}>
-                        <img key={image.id} src={image.url} alt="Random" height={"300px"} width={"400px"} />
-                        <p>
-                            <span style={{ color: "red" }}>Description:</span>{" "}
-                            <h3>{image.description}</h3>
-                        </p>
-                        <p>
-                            <span style={{ color: "red" }}>Name of picture:</span>
-                            <h3>{image.nameOfPicture}</h3>
-                        </p>
-                        <p>
-                            <span style={{ color: "red" }}>Photographer's name:</span>
-                            <h3>{image.name}</h3>
-                        </p>
+                        <Card sx={{ maxWidth: 400, boxShadow: "0 8px 16px rgba(0, 0, 0, 0.3)" }}>
+                            <CardMedia
+                                component="img"
+                                height="400"
+                                image={image.url}
+                                alt="Random"
+                            />
+                            <CardContent>
+                                {<span style={{color: "red"}}>Name: <Typography variant="body2" sx={{ fontSize: 35, fontFamily: "@fontsource/roboto/400.css" }} color="text.secondary">
+                                    {image.nameOfPicture}
+                                </Typography> </span>}
+                                <br/>
+                                {<span style={{color: "red"}}>Photo description: <Typography variant="body2" sx={{fontSize: 20}} color="text.secondary">
+                                    {image.description}
+                                </Typography></span>}
+                                <br/>
+                                {<span style={{color: "red"}}>Photographer: <Typography variant="body2" sx={{ fontSize: 20}} color="text.secondary">
+                                    {image.name}
+                                </Typography></span>}
+                            </CardContent>
+                            <CardActions style={{display: "flex", justifyContent: "space-between"}}>
+                                <Button className="share-icon" size="small" startIcon={<ShareIcon/>} onClick={sharePhoto}>Share</Button>
+                                <Button className="download-icon" size="small" href={image.url} startIcon={<DownloadIcon/>}  target="_blank">Download</Button>
+                            </CardActions>
+                        </Card>
+                        <br />
                     </Grid>
                 ))}
             </Grid>
