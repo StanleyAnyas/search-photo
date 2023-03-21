64647ab8fbe843fdcbeee84a122abcfa9837d127
Stanley Anyassor <anyassorstanley@gmail.com>
2023-03-21 09:43:22 +0200

    changed fontWeight

--- src/components/Front-page.js
+++ src/components/Front-page.js
@@ -3,6 +3,8 @@ import "../App.css";
 import InputAdornment from '@mui/material/InputAdornment';
 import TextField from '@mui/material/TextField';
 import IconButton from'@mui/material/IconButton';
+import ShareIcon from '@mui/icons-material/Share';
+import DownloadIcon from '@mui/icons-material/Download';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -10,19 +12,15 @@ import Button from '@mui/material/Button';
 import { Stack } from '@mui/system';
 import { Grid } from '@mui/material';
 import axios from "axios";
-import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
-import DescriptionIcon from '@mui/icons-material/Description';
-import LocalSeeOutlinedIcon from '@mui/icons-material/LocalSeeOutlined';
 import { Card } from '@mui/material';
 import { CardContent } from '@mui/material';
 import { CardMedia } from '@mui/material';
-//import { CardActions } from '@mui/material';
+import { CardActions } from '@mui/material';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import SvgIcon from '@mui/material/SvgIcon';
-
+import '@fontsource/roboto/900.css';
 
 
 export const FrontPage = () => { 
@@ -49,6 +47,11 @@ export const FrontPage = () => {
     const inputChanged = (event) => {
         setSearchValue(event.target.value);
       };
+
+    const sharePhoto = (imageurl, description) => {
+        const twitterUrl = `https://twitter.com/intent/tweet?text=${description}&url=${imageurl}`;
+        window.open(twitterUrl, "_blank");
+    }
 
     return (
         <div>
@@ -86,28 +89,34 @@ export const FrontPage = () => {
             <Grid container spacing={{ xs: 4, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} sx={{paddingLeft: 5, paddingBottom: 7}}>
                 {images.map((image) => (
                     <Grid item xs={4} sm={4} md={4} key={image.id}>
-                        <Card sx={{ maxWidth: 550, height: 750, boxShadow: "0 8px 16px rgba(0, 0, 0, 0.3)" }}>
+                        <Card sx={{ maxWidth: 550, height: 550, boxShadow: "0 8px 16px rgba(0, 0, 0, 0.3)" }}>
                             <CardMedia
                                 component="img"
-                                height="400"
+                                height="300"
                                 image={image.url}
                                 alt="Random"
                             />
                             <CardContent sx={{flexGrow: 1, textAlign: "left", position: "relative"}}>
-                                {<span><SvgIcon component={DriveFileRenameOutlineIcon} sx={{ fontSize: 25, color: "red" }} />
-                                 <Typography variant="body2" sx={{ fontSize: 27, fontFamily: "@fontsource/roboto/400.css", textAlign: "left" }} color="text.secondary">
+                                {<span>
+                                 <Typography variant="body2" sx={{ fontSize: 25, fontFamily: "@fontsource/roboto/900.css", textAlign: "left", color: "black", fontWeight: "bold"}} color="text.secondary">
                                     {image.nameOfPicture.length > 30 ? image.nameOfPicture.substring(0, 30) + "..." : image.nameOfPicture}
                                 </Typography> </span>}
                                 <br />
-                                {<span><SvgIcon component={DescriptionIcon} sx={{ fontSize: 25, color: "red" }} /> 
-                                <Typography variant="body2" sx={{fontSize: 20, textAlign: "left"}} color="text.secondary">
+                                {<span> 
+                                <Typography variant="body2" sx={{fontSize: 20, textAlign: "left", color:  "bold"}} color="text.secondary">
                                     {image.description.length > 40 ? image.description.substring(0, 40) + "..." : image.description}
                                 </Typography></span>}   
                                 <br /> 
-                                {<span><SvgIcon component={LocalSeeOutlinedIcon} sx={{ fontSize: 25, color: "red" }} /> 
-                                <Typography variant="body2" sx={{ fontSize: 15, textAlign: "left"}} color="text.secondary">
+                                {<span>
+                                <Typography variant="body2" sx={{ fontSize: 15, textAlign: "left", color:  "bold"}} color="text.secondary">
                                     {image.name.length > 30 ? image.name.substring(0, 30) + "..." : image.name}
                                 </Typography></span>}
+                                <div style={{ position: "absolute", bottom: -20, left: 0, right: 0 }}>
+                                    <CardActions style={{ justifyContent: "space-between", padding: "8px"}}>
+                                        <Button className="share-icon" size="small" startIcon={<ShareIcon/>} onClick={sharePhoto}>Share</Button>
+                                        <Button className="download-icon" size="small" href={image.url} startIcon={<DownloadIcon/>}  target="_blank">Download</Button>
+                                    </CardActions>
+                                </div>
                             </CardContent>
                             
                         </Card>
